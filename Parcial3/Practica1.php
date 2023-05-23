57f5c4020fe58634416dd38e7a98a6c1871f73f4
Julio442 <124410455+Julio442@users.noreply.github.com>
2023-05-22 18:59:22 -0600

    22/05/23

---
+++ Parcial3/Practica1.php
@@ -0,0 +1,23 @@
+<?php
+
+echo "<h1>Hola mundo</h1>";
+
+echo "<hr>";
+
+$nombre="Elpapushech";
+$edad= 16;
+
+if($edad >= 16){
+    echo $nombre . " es mayor";
+} else {
+    echo $nombre . " es menor";
+}
+
+echo "<br><br>";
+
+for($i=0; $i<10; $i++){
+    echo $i . "<br>";
+}
+
+
+?>
