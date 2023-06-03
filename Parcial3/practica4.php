ffc7e03fb91434338bf25a7c7d8e625ece8eb9e9
Julio442 <124410455+Julio442@users.noreply.github.com>
2023-06-02 18:43:47 -0700

    Add files via upload

--- Parcial3/practica4.php
+++ Parcial3/practica4.php
@@ -22,105 +22,108 @@ if($pregunta1=="b"){
     $calificacion++;
 }else{
     echo "<p>Respuesta es (b)</p>";
-    echo "<img src='laatipica.png' width='14%'><hr>"
+    echo "<img src='laatipica.png' width='14%'><hr>";
 }
 
 echo "<h3>pregunta 2, respuesta seleccionada: ".$pregunta2."</h3>";
 
 if($pregunta2=="b"){
     $calificacion++;
-    echo "<img src='latipica.jpeg' width='14%'><hr>"
+    echo "<img src='latipica.jpeg' width='14%'><hr>";
 }else{
     echo "<p>Respuesta es (b)</p>";
-    echo "<img src='laatipica.png' width='14%'><hr>"
+    echo "<img src='laatipica.png' width='14%'><hr>";
 }
 
 echo "<h3>pregunta 3, respuesta seleccionada: ".$pregunta3."</h3>";
 
 if($pregunta3=="a"){
     $calificacion++;
-    echo "<img src='latipica.jpeg' width='14%'><hr>"
+    echo "<img src='latipica.jpeg' width='14%'><hr>";
 }else{
     echo "<p>Respuesta es (a)</p>";
-    echo "<img src='laatipica.png' width='14%'><hr>"
+    echo "<img src='laatipica.png' width='14%'><hr>";
 }
 
 echo "<h3>pregunta 4, respuesta seleccionada: ".$pregunta4."</h3>";
 
 if($pregunta4=="a"){
     $calificacion++;
-    echo "<img src='latipica.jpeg' width='14%'><hr>"
+    echo "<img src='latipica.jpeg' width='14%'><hr>";
 }else{
     echo "<p>Respuesta es (a)</p>";
-    echo "<img src='laatipica.png' width='14%'><hr>"
+    echo "<img src='laatipica.png' width='14%'><hr>";
 }
 
 echo "<h3>pregunta 5, respuesta seleccionada: ".$pregunta5."</h3>";
 
 if($pregunta5=="c"){
     $calificacion++;
-    echo "<img src='latipica.jpeg' width='14%'><hr>"
+    echo "<img src='latipica.jpeg' width='14%'><hr>";
 }else{
     echo "<p>Respuesta es (c)</p>";
-    echo "<img src='laatipica.png' width='14%'><hr>"
+    echo "<img src='laatipica.png' width='14%'><hr>";
 }
 
 echo "<h3>pregunta 6, respuesta seleccionada: ".$pregunta6."</h3>";
 
 if($pregunta6=="b"){
     $calificacion++;
-    echo "<img src='latipica.jpeg' width='14%'><hr>"
+    echo "<img src='latipica.jpeg' width='14%'><hr>";
 }else{
     echo "<p>Respuesta es (b)</p>";
-    echo "<img src='laatipica.png' width='14%'><hr>"
+    echo "<img src='laatipica.png' width='14%'><hr>";
 }
 
 echo "<h3>pregunta 7, respuesta seleccionada: ".$pregunta7."</h3>";
 
 if($pregunta7=="c"){
     $calificacion++;
-    echo "<img src='latipica.jpeg' width='14%'><hr>"
+    echo "<img src='latipica.jpeg' width='14%'><hr>";
 }else{
     echo "<p>Respuesta es (c)</p>";
-    echo "<img src='laatipica.png' width='14%'><hr>"
+    echo "<img src='laatipica.png' width='14%'><hr>";
 }
 
 echo "<h3>pregunta 8, respuesta seleccionada: ".$pregunta8."</h3>";
 
 if($pregunta8=="c"){
     $calificacion++;
-    echo "<img src='latipica.jpeg' width='14%'><hr>"
+    echo "<img src='latipica.jpeg' width='14%'><hr>";
 }else{
     echo "<p>Respuesta es (c)</p>";
-    echo "<img src='laatipica.png' width='14%'><hr>"
+    echo "<img src='laatipica.png' width='14%'><hr>";
 }
 
 echo "<h3>pregunta 9, respuesta seleccionada: ".$pregunta9."</h3>";
 
 if($pregunta9=="b"){
     $calificacion++;
-    echo "<img src='latipica.jpeg' width='14%'><hr>"
+    echo "<img src='latipica.jpeg' width='14%'><hr>";
 }else{
     echo "<p>Respuesta es (b)</p>";
-    echo "<img src='laatipica.png' width='14%'><hr>"
+    echo "<img src='laatipica.png' width='14%'><hr>";
 }
 
 echo "<h3>pregunta 10, respuesta seleccionada: ".$pregunta10."</h3>";
 
 if($pregunta10=="c"){
     $calificacion++;
-    echo "<img src='latipica.jpeg' width='14%'><hr>"
+    echo "<img src='latipica.jpeg' width='14%'><hr>";
 }else{
     echo "<p>Respuesta es (c)</p>";
-    echo "<img src='laatipica.png' width='14%'><hr>"
+    echo "<img src='laatipica.png' width='14%'><hr>";
 }
 
 if($calificacion>=0 && $calificacion<=5){
     echo "<h3>Calificacion:".$calificacion.", No aprobado (Estas bien menso)</h3>";
+    echo "<img src='pollo.jpg' width='20%'><hr>";
 }else if($calificacion>=6 && $calificacion<=8){
     echo "<h3>Calificacion:".$calificacion.", Aprobaste (Te salvaste)</h3>";
+    echo "<img src='elao.jpg' width='20%'><hr>";
 }elseif($calificacion>=9 && $calificacion<=10){
     echo "<h3>Calificacion:".$calificacion.", Exelente (Eres mas Inteligente que el PEREZ)</h3>";
+    echo "<img src='sigma.jpg' width='20%'><hr>";
 }
 
 
